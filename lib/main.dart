829d1cfba96418d481d334b776ff3164de9ac2d6
ridhomustaqim <rm5621303@gmail.com>
2025-03-13 11:51:02 +0700

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: '/home',
       routes: {
         '/': (context) => const SplashScreen(),
         '/login': (context) => const LoginPage(),
